1550e03c32a8397c1882205a172b6a999b508fae
Debugger-ops <vivek9to5@gmail.com>
2025-08-19 01:17:09 +0530

    Delete src/models directory

--- src/models/components/layout/SuccessBox.jsx
+++
@@ -1,8 +0,0 @@
-import './successbox.css';
-export default function SuccessBox({ children }) {
-  return (
-    <div className="success-box">
-      {children}
-    </div>
-  );
-}
--- src/models/libs/dateandtime.js
+++
@@ -1,8 +0,0 @@
-export function dbTimeForHuman(str) {
-  if (!str) {
-    console.error("Invalid date string:", str);
-    return ''; // Return an empty string or some fallback value if str is undefined
-  }
-  
-  return str.replace('T', ' ').substring(0, 16);
-}
